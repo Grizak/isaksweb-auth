2c83535874a6addf5d94236afbdb62cf26e5c234
Isak Grönlund <gronlundisak11@gmail.com>
2025-10-05 08:43:19 +0200

    feat: Added username and age into user schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -11,6 +11,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
 });
 
 userSchema.pre("save", async function () {
